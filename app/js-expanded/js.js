aea455da063bb38283ff9790e605d1fde8e0b259
waldteufel <waldteufel@ukr.net>
2018-08-27 18:37:11 +0300

    jQuery-accirdion

--- app/js-expanded/js.js
+++ app/js-expanded/js.js
@@ -29,82 +29,23 @@ $(document).ready(function(){
   /* ↑↑↑ /navigation datetimer ↑↑↑ */
 
   /* ↓↓↓ accordion ↓↓↓ */
-
-  $('.accordion a').click(function(e) {
+  $('.toggle').click(function(e) {
     e.preventDefault();
 
-    var arrOfLinks = $('.accordion a');
-    for (var i = 0; i < arrOfLinks.length; i++) {                                            // визначаємо, де спрацьовує клік
-      if (this == arrOfLinks[i]) {
-        var THIS = i;
-        console.log("THIS", THIS);
-      }
+    var $this = $(this);
+
+    if ($this.next().hasClass('show')) {
+      $this.next().removeClass('show');
+      $this.next().slideUp(350);
+        //$('.toggle').children('span').children('i').css({'color':'red'});
+        alert(1);
+    } else {
+      $this.parent().parent().find('li .inner').removeClass('show');
+      $this.parent().parent().find('li .inner').slideUp(350);
+      $this.next().toggleClass('show');
+      $this.next().slideToggle(350);
     }
-
-    if (!this.isOpen) {
-
-      var linkHeight           = $('.accordion a').height() + 1;                             // висота одного посилання + 1px рамки
-      var linkQuantity         = $(this).next('ul').children('li').length;                   // кількість посилань
-      var tempHeight           = linkHeight * linkQuantity;                                  // сумарна висота посилань
-      var parentHeight         = $(this).parent().parent().height();                         // поточна висота батька
-      var newParentHeight      = tempHeight + parentHeight;                                  // нова висота батька із урахуванням висоти розкритої дитини
-      var accordionHeight      = $('.accordion').height();
-      var newAccordionHeight   = accordionHeight + tempHeight;
-
-      for (var i = 0; i < arrOfLinks.length; i++) {                                          // проходимося циклом - усе закриваємо
-        $(arrOfLinks[i]).css({'background-color':'#353535'});
-        if ($(this).parent().parent().siblings('a').children('span').length > 0) {continue}; // щоб не рухався хрестик батька, поки син відкритий
-        arrOfLinks[i].isOpen = false;
-        $(arrOfLinks[i]).siblings('ul').css({'transition':'height .5s','height': '0px'});
-        $(arrOfLinks[i]).children('span').css({'transition':'.5s','transform':'rotate(0deg)'});
-      }
-
-      $(this).css({'background-color':'black'});
-
-      $('.accordion').css({'height': accordionHeight});
-      $('.accordion').css({'transition':'.5s','height': newAccordionHeight});
-      $(this).parent().parent().css({'transition':'.5s','height': newParentHeight});
-      $(this).next('ul').css({'transition':'.5s','height': tempHeight});
-
-      $(this).children('span').css({'transition':'.5s','transform':'rotate(45deg)'});
-
-      arrOfLinks[THIS].isOpen = true;
-
-      $(".aside").css({"min-height":"730px"});
-
-      return;
-    }
-
-    if (this.isOpen) {
-
-      var parentHeight    = $(this).parent().parent().height() - $(this).next('ul').height();
-      var accordionHeight = $('.accordion').height() - $(this).next('ul').height();
-
-      $(this).next('ul').css({'transition':'.5s','height': '0px'});
-      $(this).parent().parent().css({'transition':'.5s','height': parentHeight});
-      $('.accordion').css({'transition':'.5s','height': accordionHeight});
-      $(this).siblings('ul').children('li').children('ul').css({'height': '0px'});
-
-      $(this).children('span').css({'transition':'.5s','transform':'rotate(0deg)'});
-      $(this).siblings('ul').children('li').children('a').children('span').css({'transition':'.5s','transform':'rotate(0deg)'});
-
-      arrOfLinks[THIS].isOpen = false;
-      arrOfLinks[21].isOpen = false;                                                         // не камільфо, але ситуацію рятує
-
-      $(".aside").css({"min-height":"610px"});
-
-      return;
-    }
-
   });
-
-// info
-// при відкритті нижніх підпунктів пунктів меню висота бокової панелі
-// не збільшувалася, менюшки, які не влазили в панель були не видимі,
-// work-area також не доходила до низу вікна браузера. Тому був
-// доданий код зі зміною висоти aside:
-// $(".aside").css({"min-height":"nnnnnn"});
-
   /* ↑↑↑ /accordion ↑↑↑ */
 
 });
